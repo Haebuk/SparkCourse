444569c4ee6e1c30fb7bee6ffd79124cf7b32225
Jaesung Ryu <cwryu6252@naver.com>
2022-04-05 23:21:50 +0900

    2-14

--- ratings-counter.py
+++ ratings-counter.py
@@ -4,8 +4,8 @@ import collections
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("~/../../SparkCourse/ml-100k/u.data")
-ratings = lines.map(lambda x: x.split()[2])
+lines = sc.textFile("ml-100k/u.data")
+ratings = lines.map(lambda x: x. split()[2])
 result = ratings.countByValue()
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
